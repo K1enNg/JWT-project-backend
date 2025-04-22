9d464c843bb3cbbd3c73f59bbda3c9f3e8872078
Kinkin <hiimkien2702@gmail.com>
2025-04-22 08:59:31 -0400

    validated email

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,25 @@
-import { IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateUserDto {
-    @IsNotEmpty()
+    @IsNotEmpty({message: 'Name is required'})
     name: string;
+
+    @IsNotEmpty({message: 'Email is required'})
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty({message: 'Password is required'})
     password: string;
+
+    @IsOptional()
+    @IsString()
     phone: string;
+
+    @IsOptional()
+    @IsString()
     address: string;
+
+    @IsOptional()
+    @IsString()
     image: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { hashPassword } from 'src/utils/utils';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>
+  ) { }
+  isEmailExist = async (email: string) => {
+    const user = await this.userModel.findOne({ email });
+    if (user) return true;
+    return false;
+  }
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+    const isExist = await this.isEmailExist(email);
+    if (isExist) {
+      throw new BadRequestException('Email already exists. Please use another email');
+    }
+    const hash = await hashPassword(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hash,
+      phone,
+      address,
+      image
+    });
+    return {
+      _id: user._id
+    };
   }
 
   findAll() {
@@ -24,3 +53,4 @@ export class UsersService {
     return `This action removes a #${id} user`;
   }
 }
+
