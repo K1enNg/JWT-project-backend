2deb7d5fb805a7ee7b693c1841e3d570c104b4f2
Kinkin <hiimkien2702@gmail.com>
2025-05-09 13:58:38 -0400

    sent email with node mailer

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,14 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { Public } from 'src/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,
+    private readonly mailerService: MailerService
+  ) 
+  {}
   
   @Post("login")
   @Public()
@@ -19,5 +23,20 @@ export class AuthController {
   @Public()
   register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get('send-email')
+  @Public()
+  testEmail() 
+  {
+    this.mailerService
+      .sendMail({
+        to: 'ericdubois1206@gmail.com', // list of receivers
+        from: 'noreply@nestjs.com', // sender address
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>Welcome</b>', // HTML body content
+      })
+    return "Hello World";
   }
 }
